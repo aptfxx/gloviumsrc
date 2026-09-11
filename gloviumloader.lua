af1b9c45c1aad88899f438350fb013b2ffc8b185
aptitude <167242158+aptfxx@users.noreply.github.com>
2026-09-11 22:42:01 +0100

    Modify game ID and variable for Arsenal support
    Updated the supported game ID for Arsenal and improved variable naming.

--- gloviumloader.lua
+++ gloviumloader.lua
@@ -14,15 +14,30 @@ Glovy This, Glovy That
 
 ]]
 
+ascii = [[
+      ::::::::  :::        ::::::::  :::     ::: ::::::::::: :::    :::   :::   ::: 
+    :+:    :+: :+:       :+:    :+: :+:     :+:     :+:     :+:    :+:  :+:+: :+:+: 
+   +:+        +:+       +:+    +:+ +:+     +:+     +:+     +:+    +:+ +:+ +:+:+ +:+ 
+  :#:        +#+       +#+    +:+ +#+     +:+     +#+     +#+    +:+ +#+  +:+  +#+  
+ +#+   +#+# +#+       +#+    +#+  +#+   +#+      +#+     +#+    +#+ +#+       +#+   
+#+#    #+# #+#       #+#    #+#   #+#+#+#       #+#     #+#    #+# #+#       #+#    
+########  ########## ########      ###     ###########  ########  ###       ###     
+                                                  - Glovium is so sick
+]]
+warn(ascii)
+
+
+print("[GloviumLoader] Checking your game...")
 local gameId = game.GameId
 local supportedGames = {
-   [286090429] = "Arsenal",
+   [111958650] = "Arsenal",
 }
-
+print("[GloviumLoader] Game Checked!")
 if supportedGames[gameId] then
     print("[GloviumLoader] Your game is supported!")
-    local game = supportedGames[gameId]
-    local loadurl = string.format("https://raw.githubusercontent.com/aptfxx/gloviumsrc/refs/heads/main/games/%s.lua", game)
+    local impropGame = supportedGames[gameId]
+    local loadurl = string.format("https://raw.githubusercontent.com/aptfxx/gloviumsrc/refs/heads/main/games/%s.lua", impropGame)
     if game:HttpGet(loadurl) then print("[GloviumLoader] URL formatted, loading Glovium.") end
     loadstring(game:HttpGet(loadurl))()
+    print("[GloviumLoader] Glovium has finished loading!")
 end
