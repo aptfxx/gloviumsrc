76b8367057d52ffd2b29d5b472aa9f49d86ff2f3
aptitude <167242158+aptfxx@users.noreply.github.com>
2026-09-11 22:29:47 +0100

    GloviumLoader v1
    Added support for loading game scripts from Glovium.

--- gloviumloader.lua
+++ gloviumloader.lua
@@ -1,1 +1,28 @@
-print("worker working")
+--[[
+  ▄████  ██▓     ▒█████   ██▒   █▓ ██▓ █    ██  ███▄ ▄███▓
+ ██▒ ▀█▒▓██▒    ▒██▒  ██▒▓██░   █▒▓██▒ ██  ▓██▒▓██▒▀█▀ ██▒
+▒██░▄▄▄░▒██░    ▒██░  ██▒ ▓██  █▒░▒██▒▓██  ▒██░▓██    ▓██░
+░▓█  ██▓▒██░    ▒██   ██░  ▒██ █░░░██░▓▓█  ░██░▒██    ▒██ 
+░▒▓███▀▒░██████▒░ ████▓▒░   ▒▀█░  ░██░▒▒█████▓ ▒██▒   ░██▒
+ ░▒   ▒ ░ ▒░▓  ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░
+  ░   ░ ░ ░ ▒  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░░▒░ ░ ░ ░  ░      ░
+░ ░   ░   ░ ░   ░ ░ ░ ▒       ░░   ▒ ░ ░░░ ░ ░ ░      ░   
+      ░     ░  ░    ░ ░        ░   ░     ░            ░   
+                              ░                           
+
+Glovy This, Glovy That
+
+]]
+
+local gameId = game.GameId
+local supportedGames = {
+   [286090429] = "Arsenal",
+}
+
+if supportedGames[gameId] then
+    print("[GloviumLoader] Your game is supported!")
+    local game = supportedGames[gameId]
+    local loadurl = string.format("https://raw.githubusercontent.com/aptfxx/gloviumsrc/refs/heads/main/games/%s.lua", game)
+    if game:HttpGet(loadurl) then print("[GloviumLoader] URL formatted, loading Glovium.") end
+    loadstring(game:HttpGet(loadurl))()
+end
