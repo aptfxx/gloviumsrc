bad8e8532be355add85436060f1f1458a3753cb5
aptitude <167242158+aptfxx@users.noreply.github.com>
2026-09-12 13:34:52 +0100

    Improve error handling for game script loading
    Added error handling for URL formatting issues when loading game scripts.

--- gloviumloader.lua
+++ gloviumloader.lua
@@ -40,7 +40,15 @@ if supportedGames[gameId] then
     print("[GloviumLoader] Your game is supported!")
     local impropGame = supportedGames[gameId]
     local loadurl = string.format("https://raw.githubusercontent.com/aptfxx/gloviumsrc/refs/heads/main/games/%s.lua", impropGame)
-    if game:HttpGet(loadurl) then print("[GloviumLoader] URL formatted, loading Glovium.") end
+    if game:HttpGet(loadurl) then print("[GloviumLoader] URL formatted, loading Glovium.") else error("[GloviumLoader] URL Did not format correctly. Check if this game's script file is present.") end
     loadstring(game:HttpGet(loadurl))()
+    print("[GloviumLoader] Glovium has finished loading!")
+else
+    warm("[GloviumLoader] Your game is not supported!")
+    print("[GloviumLoader] Loading Universal...")
+    local impropGame = "universal"
+    local loadurl = string.format("https://raw.githubusercontent.com/aptfxx/gloviumsrc/refs/heads/main/games/%s.lua", impropGame)
+    if game:HttpGet(loadurl) then print("[GloviumLoader] URL formatted, loading Glovium.") else error("[GloviumLoader] URL Did not format correctly. Check if this game's script file is present.") end
+     loadstring(game:HttpGet(loadurl))()
     print("[GloviumLoader] Glovium has finished loading!")
 end
