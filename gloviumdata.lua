0bd42b9413fcbc108cb1044819aa75d8a6287075
aptitude <167242158+aptfxx@users.noreply.github.com>
2026-09-15 21:52:54 +0100

    Create gloviumdata.lua

---
+++ gloviumdata.lua
@@ -0,0 +1,24 @@
+local gloviumdata = {}
+
+gloviumdata.GameShortNames = {
+  [111958650] = "Arsenal",
+  [10549097626] = "ShootABrick",
+  [9691480368] = "GrnG",
+  [10561206188] = "Verity",
+}
+
+gloviumdata.GameFullNames = {
+  [111958650] = "Arsenal",
+  [10549097626] = "Shoot A Brick",
+  [9691480368] = "GrnG",
+  [10561206188] = "+1 Speed Verity Escape",
+}
+
+gloviumdata.GameLinks = {
+  [111958650] = "https://www.roblox.com/games/286090429",
+  [10549097626] = "https://www.roblox.com/games/84393169213297",
+  [9691480368] = "https://www.roblox.com/games/83534484104746",
+  [10561206188] = "https://www.roblox.com/games/115875360612307",
+}
+
+gloviumdata.GameCount = 4
