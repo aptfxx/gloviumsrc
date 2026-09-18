604908cae7293ba62d4ca38bde4a7ec659997a3b
aptitude <167242158+aptfxx@users.noreply.github.com>
2026-09-19 00:29:56 +0100

    Update gloviumdata.lua

--- gloviumdata.lua
+++ gloviumdata.lua
@@ -5,6 +5,7 @@ gloviumdata.GameShortNames = {
   [10549097626] = "ShootABrick",
   [9691480368] = "GrnG",
   [10561206188] = "Verity",
+  [66654135] = "MM2",
 }
 
 gloviumdata.GameFullNames = {
@@ -12,6 +13,7 @@ gloviumdata.GameFullNames = {
   [10549097626] = "Shoot A Brick",
   [9691480368] = "GrnG",
   [10561206188] = "+1 Speed Verity Escape",
+  [66654135] = "Murder Mystery 2",
 }
 
 gloviumdata.GameLinks = {
@@ -19,6 +21,7 @@ gloviumdata.GameLinks = {
   [10549097626] = "https://www.roblox.com/games/84393169213297",
   [9691480368] = "https://www.roblox.com/games/83534484104746",
   [10561206188] = "https://www.roblox.com/games/115875360612307",
+  [66654135] = "https://www.roblox.com/games/142823291"
 }
 
-gloviumdata.GameCount = 4
+gloviumdata.GameCount = 5
